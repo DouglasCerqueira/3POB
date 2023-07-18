2708ac4aa8ca5aba7ae8385f998bdfb8ff86f844
Douglas Alves <112496849+DouglasCerqueira@users.noreply.github.com>
2023-07-18 16:35:57 -0300

    Add files via upload

---
+++ ORACLE/Exercicio 1 - ORACLE/src/main.java
@@ -0,0 +1,25 @@
+public class main {
+
+    /*public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } */
+
+    /* passo 1: Edite o código de modo que ele imprima uma mensagem diferente de “Hello World!”.
+    - Compile e execute o código para verificar o efeito das alterações que foram feitas. */
+
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+
+    // Resposta: Ele imprime outra mensagem, ou seja, a mensagem que foi escrita dentro do codigo.
+
+    /* passo 2:	Altere o ‘S’ em ‘System’ para minúsculo e tente compilar.
+•	-  A sintaxe Java faz distinção entre letras maiúsculas e minúsculas? */
+
+    // Resposta: Não. Com a letra 'S' em 'System' minúscula, o java diz 'package system does not exist'.
+
+    /* passo3 :Remova o ponto e vírgula (;) e tente compilar.
+•	- O ponto e vírgula é importante para a sintaxe Java? */
+
+    // Resposta: Sim, senão o java nao sabe aonde parar para prosseguir para a proxima linha
+}
