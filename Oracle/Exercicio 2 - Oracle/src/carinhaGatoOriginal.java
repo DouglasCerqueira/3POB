00282a48b290e371a8a50e801002fbc2e5193fa6
Douglas Alves <112496849+DouglasCerqueira@users.noreply.github.com>
2023-07-18 19:41:44 -0300

    Add files via upload

---
+++ Oracle/Exercicio 2 - Oracle/src/carinhaGatoOriginal.java
@@ -0,0 +1,12 @@
+public class carinhaGatoOriginal {
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\");
+        System.out.println("  /  \\_______/  \\");
+        System.out.println(" /               \\");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====    \\/     ====");
+        System.out.println("======(__|__)======");
+        System.out.println(" (               )");
+        System.out.println("  (_____________)");
+    }
+}
